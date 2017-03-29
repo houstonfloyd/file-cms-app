35e0a2debd6f4f9952a8e0d00a1c72f09a39216c
Andrew Ubuntu <andrew.houstonfloyd@gmail.com>
2017-03-29 18:03:50 -0400

    Final commit

--- cms.rb
+++ cms.rb
@@ -4,6 +4,8 @@ require 'sinatra/content_for'
 require 'pry'
 require 'tilt/erubis'
 require 'redcarpet'
+require 'yaml'
+require 'bcrypt'
 
 configure do
   enable :sessions
@@ -48,6 +50,97 @@ def load_file_content(file)
   end
 end
 
+def name_error(file_name)
+  if file_name.strip.empty?
+    "#A name is required."
+  end
+end
+
+def load_user_credentials
+  credentials_path = if ENV["RANK_ENV"] == 'test'
+    File.expand_path("../test/users.yml", __FILE__)
+  else
+    File.expand_path("../users.yml", __FILE__)
+  end
+
+  YAML.load_file(credentials_path)
+end
+
+def valid_credentials?(username, password)
+  credentials = load_user_credentials
+
+  if credentials.key?(username)
+    bcrypt_password = BCrypt::Password.new(credentials[username])
+    crypt_password == password
+  else
+    false
+  end
+end
+
+def invalid_user
+  credentials = load_user_credentials
+  username = session[:username]
+
+  credentials.key?(username) && credentials[username] == params[:password]
+end
+
+def require_signed_in_user
+  if invalid_user
+    session[:message] = "You must be signed in to do that."
+    redirect '/'
+  end
+end
+
+get '/new' do
+  require_signed_in_user
+
+  erb :new_file
+end
+
+get '/users/signin' do
+
+  erb :signin
+end
+
+post '/users/signin' do
+  username = params[:username]
+
+  if valid_credentials?(username, params[:password])
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect '/'
+  else
+    session[:message] = "Invalid Credentials"
+    status 422
+    erb :signin
+  end
+end
+
+post '/users/signout' do
+  session[:username] = nil
+  session[:message] = "You have been signed out."
+  redirect '/'
+end
+
+post '/' do
+  require_signed_in_user
+  file_name = params[:file_name]
+  error = name_error(file_name)
+
+  file_name = file_name + '.txt' if !File.basename(file_name).include? '.'
+  file_path = File.join(data_path, file_name)
+
+  if error
+    session[:message] = error
+    status 422
+    erb :new_file
+  else
+    File.write(file_path, '')
+    session[:message] = "#{params[:file_name]} has been created."
+    redirect '/'
+  end
+end
+
 get '/:file_name' do
   file_name = params[:file_name]
   file_path = File.join(data_path, file_name)
@@ -61,6 +154,7 @@ get '/:file_name' do
 end
 
 get '/:file_name/edit' do
+  require_signed_in_user
   @file_name = params[:file_name]
   file_path = File.join(data_path, @file_name)
   @content = File.read(file_path)
@@ -68,7 +162,20 @@ get '/:file_name/edit' do
   erb :edit_file
 end
 
+post '/:file_name/delete' do
+  require_signed_in_user
+
+  @file_name = params[:file_name]
+  file_path = File.join(data_path, @file_name)
+
+  File.delete(file_path)
+  session[:message] = "#{params[:file_name]} has been deleted."
+  redirect '/'
+end
+
 post "/:file_name" do
+  require_signed_in_user
+
   file_path = File.join(data_path, params[:file_name])
   content = params[:content]
 
--- test/app_test.rb
+++ test/app_test.rb
@@ -10,6 +10,15 @@ ENV["RACK_ENV"] = "test" #This value is used by various parts
 # the case of Sinatra, determine whether it will start a web
 # server or not (we don't want to if we're running tests)
 
+#Requests and responses in Rack are associated with a large Hash of data 
+#related to a request-response pair, called the "env" by Rack internally. 
+#Some of the values in this hash are used by frameworks such as Sinatra 
+#and Rails to access the path, parameters, and other attributes of the 
+#request. The session implementation used by Sinatra is actually supplied 
+#by Rack, and as a result the session object also lives in this Hash. 
+#To access it within a test, we can use last_request.env. Note that the 
+#last_request method is used here and not last_response.
+
 require "minitest/autorun" #these are the two libraries we need
 require "rack/test"        #for testing
 require "fileutils"        #necessary to make test dir/files for tests
@@ -30,6 +39,14 @@ class AppTest < Minitest::Test #test methods need to be defined in in a class th
 
   def teardown
     FileUtils.rm_rf(data_path)
+  end
+
+  def session
+    last_request.env["rack.session"]
+  end
+
+  def admin_session
+    { "rack.session" => { username: "admin" } }
   end
 
   def create_document(name, content = "")
@@ -60,15 +77,11 @@ class AppTest < Minitest::Test #test methods need to be defined in in a class th
     assert_includes last_response.body, "Ruby copyright 2017"
   end
 
-  def test_unknown_file
-    get '/other.txt'
+  def test_document_not_found
+    get "/notafile.ext"
 
     assert_equal 302, last_response.status
-    
-    get last_response["Location"] #I used get '/' which did the same I think
-    
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "does not exist"
+    assert_equal "notafile.ext does not exist.", session[:message]
   end
 
   def test_markdown_render
@@ -84,25 +97,128 @@ class AppTest < Minitest::Test #test methods need to be defined in in a class th
   def test_editing_document
     create_document "changes.txt"
 
-    get '/changes.txt/edit'
+    get '/changes.txt/edit', {}, admin_session
     
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<textarea"
     assert_includes last_response.body, %q(<button type="submit")
   end
 
-  def test_edit_content
-    post '/changes.txt', content: "new content"
+  def test_editing_document_signed_out
+    create_document "changes.txt"
+
+    get "/changes.txt/edit"
 
     assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
+  end
 
-    get last_response["Location"]
+  def test_updating_document
+    post "/changes.txt", content: "new content"
 
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "has been updated"
+    assert_equal 302, last_response.status
+    assert_equal "changes.txt has been updated.", session[:message]
 
     get "/changes.txt"
     assert_equal 200, last_response.status
     assert_includes last_response.body, "new content"
   end
+
+
+  def test_view_new_document_form
+    get "/new"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  # def test_create_new_document
+  #   post "/", filename: "test.txt"
+  #   assert_equal 302, last_response.status
+  #   assert_equal "test.txt has been created.", session[:message]
+
+  #   get "/"
+  #   assert_includes last_response.body, "test.txt"
+  # end
+
+  def test_deleting_document
+    create_document("test.txt")
+
+    post "/test.txt/delete"
+    assert_equal 302, last_response.status
+    assert_equal "test.txt has been deleted.", session[:message]
+
+    get "/"
+    refute_includes last_response.body, %q(href="/test.txt")
+  end
+
+  def test_signin_form
+    get '/users/signin'
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_signin
+    post "/users/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    assert_equal "admin", session[:username]
+    assert_equal "Welcome!", session[:message]
+
+    get last_response['Location']
+    assert_includes last_response.body, "Signed in as admin"
+  end
+
+ # def test_signin_with_bad_credentials
+ #    post "/users/signin", username: "guest", password: "shhhh"
+ #    assert_equal 422, last_response.status
+ #    assert_nil, session[:username]
+ #    assert_includes last_response.body, "Invalid credentials"
+ #  end
+
+  def test_signout
+    get "/", {}, {"rack.session" => {username: "admin"} }
+    assert_includes last_response.body, "Signed in as admin"
+
+    post "/users/signout"
+    get last_response["Location"]
+
+    #assert_nil, session[:username]
+    assert_includes last_response.body, "You have been signed out"
+    assert_includes last_response.body, "Sign In"
+  end
+
+  # After adding the session method to access the last request's env, 
+  #within a test, you can make assertions about the values 
+  #within a session:
+
+  # def test_sets_session_value
+  #   get "/path_that_sets_session_value"
+  #   assert_equal "expected value", session[:key]
+  # end
+
+  #If you need to go the other way (set a value in the session before 
+  #a request is made), Rack::Test allows values for the Rack.env hash 
+  #to be provided to calls to get and post within a test. So a simple 
+  #request like this one:
+
+  # def test_index
+  #   get "/"
+  # end
+
+  #becomes this:
+
+  def test_index_as_signed_in_user
+    #two hash args: first is params (in this case empty), 2nd is
+    #values to be added to the request's Rack.env hash
+    get "/", {}, {"rack.session" => { username: "admin"} }
+  end
+
+  #Once values have been provided like this once, they will be remembered 
+  #for all future calls to get or post within the same test, unless, 
+  #of course, those values are modified by code within your application. 
+  #This means that you can set values for the session in the first request 
+  #made in a test and they will be retained until you remove them.
 end
